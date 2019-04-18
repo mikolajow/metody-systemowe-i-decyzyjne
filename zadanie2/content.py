dbf06c9e31a76335da747feafb126867b7beb66b
PAPAJ <mikolajkasperek@o2.pl>
2019-04-18 10:33:09 +0200

    zad 2

--- zadanie2/content.py
+++ zadanie2/content.py
@@ -8,6 +8,7 @@
 
 from __future__ import division
 import numpy as np
+import collections
 
 
 def hamming_distance(X, X_train):
@@ -19,35 +20,25 @@ def hamming_distance(X, X_train):
     zbioru zwrocone zostana w postaci macierzy
     :return: macierz odleglosci pomiedzy obiektami z X i X_train N1xN2
     """
-    #print(X)
-    #print("@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@")
-    #print(X_train)
-
     x_decompresed = X.todense().astype(int)
     x_decompresed_negated = (~(X.todense())).astype(int)
 
     x_train_decompresed_negated = (~(X_train.todense())).astype(int)
     x_train_decompresed = X_train.todense().astype(int)
 
+    #print(x_decompresed.shape)
+    #print(x_train_decompresed.shape)
+
     multiplied = x_decompresed @ (x_train_decompresed.transpose())
     negated_multiplayed = x_decompresed_negated @ (x_train_decompresed_negated.transpose())
-
-    # print(multiplied)
-    # print("@@@@@@@@@@@@@@@@@@@@@@@@@@")
-    # print(negated_multiplayed)
-    # print("@@@@@@@@@@@@@@@@@@@@@@@@@@")
-    # print(multiplied.shape)
-    # print(negated_multiplayed.shape)
 
     similarity_matrinx = multiplied + negated_multiplayed
 
     dimensions_matrix = x_decompresed.shape[1] * np.ones(similarity_matrinx.shape)
 
-    # print(dimensions_matrix.shape)
+    #print(dimensions_matrix.shape)
 
     return dimensions_matrix - similarity_matrinx
-
-
 
 
 def sort_train_labels_knn(Dist, y):
@@ -65,7 +56,11 @@ def sort_train_labels_knn(Dist, y):
     wartosci podobienstw odpowiadajacego wiersza macierzy
     Dist. Uzyc algorytmu mergesort.
     """
-    pass
+    #print(Dist.shape)
+    #print(y)
+    result = y[Dist.argsort(kind='mergesort')]
+    #print(result)
+    return result
 
 
 def p_y_x_knn(y, k):
@@ -77,7 +72,26 @@ def p_y_x_knn(y, k):
     :param k: liczba najblizszuch sasiadow dla KNN
     :return: macierz prawdopodobienstw dla obiektow z X
     """
-    pass
+    # print(y.shape)
+    #print(y)
+    # print(k)
+    categories = set(y[0])
+    # print(categories)
+    # print(number_of_categories)
+    result = []
+    for row in y:
+        result_row = []
+        first_k_indexes = row[0:k]
+        for i in range(len(categories)):
+            result_row.append(np.count_nonzero(first_k_indexes == i+1))
+            # print(result_row)
+        result.append(result_row)
+    result = np.array(result) / k
+    #print(result)
+    return result
+
+
+
 
 
 def classification_error(p_y_x, y_true):
@@ -88,7 +102,26 @@ def classification_error(p_y_x, y_true):
     Kazdy wiersz macierzy reprezentuje rozklad p(y|x)
     :return: blad klasyfikacji
     """
-    pass
+    #print(y_true.shape)
+    errors = 0
+    row_index = 0
+    for row in p_y_x:
+        biggest_prob = 0.0
+        category_index = 0
+        best_category = 0
+        for prob_index in range(row.size):
+            if biggest_prob <= row[prob_index]:
+                biggest_prob = row[prob_index]
+                best_category = category_index + 1
+                #print(best_category)
+            category_index += 1
+        if best_category != y_true[row_index]:
+            errors += 1
+        row_index += 1
+    #print(y_true.size)
+    #print(errors/y_true.size)
+    return errors/y_true.size
+
 
 
 def model_selection_knn(Xval, Xtrain, yval, ytrain, k_values):
@@ -101,7 +134,24 @@ def model_selection_knn(Xval, Xtrain, yval, ytrain, k_values):
     :return: funkcja wykonuje selekcje modelu knn i zwraca krotke (best_error,best_k,errors), gdzie best_error to najnizszy
     osiagniety blad, best_k - k dla ktorego blad byl najnizszy, errors - lista wartosci bledow dla kolejnych k z k_values
     """
-    pass
+    #print(Xval.shape)
+    #print(k_values)
+    #print(yval)
+    #print(ytrain)
+
+    distance_matrix = hamming_distance(Xval, Xtrain)
+    sorted_distance = sort_train_labels_knn(distance_matrix, ytrain)
+    errors = []
+    for k in k_values:
+        #print(k)
+        probability_matrix = p_y_x_knn(sorted_distance, k)
+        error = classification_error(probability_matrix, yval)
+        errors.append(error)
+    #print(errors)
+    min_error = min(errors)
+    min_error_index = errors.index(min_error)
+    return min_error, k_values[min_error_index], errors
+
 
 
 def estimate_a_priori_nb(ytrain):
@@ -109,7 +159,12 @@ def estimate_a_priori_nb(ytrain):
     :param ytrain: etykiety dla dla danych treningowych 1xN
     :return: funkcja wyznacza rozklad a priori p(y) i zwraca p_y - wektor prawdopodobienstw a priori 1xM
     """
-    pass
+    categories_set = set(ytrain)
+    categories_count = []
+    for i in range(len(categories_set)):
+        categories_count.append(np.count_nonzero(ytrain == i+1))
+    return np.array(categories_count)/ytrain.shape[0]
+
 
 
 def estimate_p_x_y_nb(Xtrain, ytrain, a, b):
@@ -121,7 +176,34 @@ def estimate_p_x_y_nb(Xtrain, ytrain, a, b):
     :return: funkcja wyznacza rozklad prawdopodobienstwa p(x|y) zakladajac, ze x przyjmuje wartosci binarne i ze elementy
     x sa niezalezne od siebie. Funkcja zwraca macierz p_x_y o wymiarach MxD.
     """
-    pass
+    x_decompresed = Xtrain.todense().astype(int)
+
+    up_factor = a - 1.0
+    down_factor = a + b - 2.0
+    categories_set = set(ytrain)
+    categories_count = []
+    for i in range(len(categories_set)):
+        categories_count.append(np.count_nonzero(ytrain == i+1))
+
+    probabilities_matrix = np.zeros(shape=(len(categories_set), x_decompresed.shape[1]))
+
+    for row_index in range(x_decompresed.shape[0]):
+        prob_matrix_row_index = ytrain[row_index] -1
+        for word_index in range(x_decompresed[row_index].size):
+            if x_decompresed[row_index, word_index] == 1:
+                probabilities_matrix[prob_matrix_row_index, word_index] += 1
+
+    for row_index in range(probabilities_matrix.shape[0]):
+        for column_index in range(probabilities_matrix.shape[1]):
+            probabilities_matrix[row_index, column_index] = (probabilities_matrix[row_index, column_index] + up_factor)\
+                                                            / (categories_count[row_index] + down_factor)
+    return probabilities_matrix
+
+
+
+
+
+
 
 
 def p_y_x_nb(p_y, p_x_1_y, X):
@@ -132,7 +214,18 @@ def p_y_x_nb(p_y, p_x_1_y, X):
     :return: funkcja wyznacza rozklad prawdopodobienstwa p(y|x) dla kazdej z klas z wykorzystaniem klasyfikatora Naiwnego
     Bayesa. Funkcja zwraca macierz p_y_x o wymiarach NxM.
     """
-    pass
+    x = X.todense().astype(int)
+
+    prob_for_class = []
+    prob_word_in_class = p_x_1_y
+
+    prob_word_not_in_class = 1 - prob_word_in_class
+    x_not = 1 - x
+    
+
+
+
+
 
 
 def model_selection_nb(Xtrain, Xval, ytrain, yval, a_values, b_values):
@@ -148,4 +241,6 @@ def model_selection_nb(Xtrain, Xval, ytrain, yval, a_values, b_values):
     osiagniety blad, best_a - a dla ktorego blad byl najnizszy, best_b - b dla ktorego blad byl najnizszy,
     errors - macierz wartosci bledow dla wszystkich par (a,b)
     """
+
+
     pass
