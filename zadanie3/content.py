6a8b870bc767c622a8806fab169ed397f758bd8a
PAPAJ <mikolajkasperek@o2.pl>
2019-05-12 01:00:46 +0200

    raz lista... a raz nie ;p

--- zadanie3/content.py
+++ zadanie3/content.py
@@ -16,7 +16,7 @@ def sigmoid(x):
     :param x: wektor wartości *x* do zaaplikowania funkcji sigmoidalnej Nx1
     :return: wektor wartości funkcji sigmoidalnej dla wartości *x* Nx1
     """
-    pass
+    return 1/(1+np.exp(-x))
 
 
 def logistic_cost_function(w, x_train, y_train):
@@ -29,7 +29,17 @@ def logistic_cost_function(w, x_train, y_train):
     :return: krotka (log, grad), gdzie *log* to wartość funkcji logistycznej,
         a *grad* jej gradient po parametrach *w* Mx1
     """
-    pass
+    N = x_train.shape[0]
+    sigmoid_matrix = sigmoid(x_train @ w)
+    log = 0.0
+
+    for i in range(N):
+        log += y_train[i] * np.log(sigmoid_matrix[i]) + (1 - y_train[i]) * np.log(1 - sigmoid_matrix[i])
+    log = log/N*(-1)
+    gradient = 1/N*(np.transpose(x_train) @ (sigmoid_matrix - y_train))
+    return log, gradient
+
+
 
 
 def gradient_descent(obj_fun, w0, epochs, eta):
@@ -48,7 +58,17 @@ def gradient_descent(obj_fun, w0, epochs, eta):
         punkt *w*, a *log_values* to lista wartości funkcji celu w każdej
         epoce (lista o długości *epochs*)
     """
-    pass
+    log_values = np.zeros(epochs)
+    w = w0.copy()
+    for i in range(epochs):
+        _, grad = obj_fun(w)
+        delta_w = -grad
+        w += eta*delta_w
+        val, _ = obj_fun(w)
+        log_values[i] = val
+    return w, log_values
+
+
 
 
 def stochastic_gradient_descent(obj_fun, x_train, y_train, w0, epochs, eta, mini_batch):
@@ -71,8 +91,22 @@ def stochastic_gradient_descent(obj_fun, x_train, y_train, w0, epochs, eta, mini
         punkt *w*, a *log_values* to lista wartości funkcji celu dla całego
         zbioru treningowego w każdej epoce (lista o długości *epochs*)
     """
-    pass
+    w = w0.copy()
+    M = (int)(x_train.shape[0] / mini_batch)
+    log_values = np.zeros(epochs)
 
+    for k in range(epochs):
+        for mini_b_number in range(M):
+            x_mini_batch = x_train[mini_b_number*mini_batch : mini_b_number*mini_batch + mini_batch]
+            y_mini_batch = y_train[mini_b_number*mini_batch : mini_b_number*mini_batch + mini_batch]
+
+            _, grad = obj_fun(w, x_mini_batch, y_mini_batch)
+            delta_w = -grad
+            w += delta_w * eta
+        # dla całego zbioru danych a nie poszczególnych mini_batchów
+        val, _ = obj_fun(w, x_train, y_train)
+        log_values[k] = val
+    return w, log_values
 
 def regularized_logistic_cost_function(w, x_train, y_train, regularization_lambda):
     """
@@ -86,8 +120,14 @@ def regularized_logistic_cost_function(w, x_train, y_train, regularization_lambd
     :return: krotka (log, grad), gdzie *log* to wartość funkcji logistycznej
         z regularyzacją l2, a *grad* jej gradient po parametrach *w* Mx1
     """
-    pass
+    logistic_without_reg, grad = logistic_cost_function(w, x_train, y_train)
+    w_copy = w.copy()
+    w_copy[0] = 0
+    regularization = regularization_lambda/2 * (np.transpose(w_copy) @ w_copy)
 
+    log = logistic_without_reg + regularization
+    grad_with_reg = grad + regularization_lambda * w_copy
+    return log, grad_with_reg
 
 def prediction(x, w, theta):
     """
@@ -99,8 +139,15 @@ def prediction(x, w, theta):
     :param theta: próg klasyfikacji z przedziału [0,1]
     :return: wektor predykowanych etykiet ze zbioru {0, 1} Nx1
     """
-    pass
-
+    #result = [ [1] if sigmoid(np.transpose(w) @ row) >= theta else [0] for row in x ]
+    #print(result)
+    result = np.zeros((x.shape[0],1))
+    for i in range(x.shape[0]):
+        if sigmoid(np.transpose(w) @ x[i]) >= theta:
+            result[i] = 1
+        else:
+            result[i] = 0
+    return result
 
 def f_measure(y_true, y_pred):
     """
@@ -111,7 +158,10 @@ def f_measure(y_true, y_pred):
     :param y_pred: wektor etykiet predykowanych przed model Nx1
     :return: wartość miary F (F-measure)
     """
-    pass
+    true_positives = np.sum(y_true * y_pred)
+    model_mistakes = np.sum(np.logical_xor(y_pred, y_true))
+
+    return 2*true_positives/(2*true_positives + model_mistakes)
 
 
 def model_selection(x_train, y_train, x_val, y_val, w0, epochs, eta, mini_batch, lambdas, thetas):
@@ -139,4 +189,29 @@ def model_selection(x_train, y_train, x_val, y_val, w0, epochs, eta, mini_batch,
         *w* to parametry najlepszego modelu, a *F* to macierz wartości miary F
         dla wszystkich par *(lambda, theta)* #lambda x #theta
     """
-    pass
+
+    F_matrix = np.zeros((len(lambdas), len(thetas)))
+    best_f = 0
+    best_w = 0
+    best_theta = 0
+    best_lambda = 0
+    for lambd in range(len(lambdas)):
+        obj_fun = lambda w, x_tr, y_tr: regularized_logistic_cost_function(w, x_tr, y_tr, lambdas[lambd])
+        current_w, _ = stochastic_gradient_descent(obj_fun, x_train, y_train, w0, epochs, eta, mini_batch)
+        for theta in range(len(thetas)):
+            f = f_measure(y_val, prediction(x_val, current_w, thetas[theta]))
+            F_matrix[lambd, theta] = f
+            if f > best_f:
+                best_f = f
+                best_theta = thetas[theta]
+                best_lambda = lambdas[lambd]
+                best_w = current_w
+    return best_lambda, best_theta, best_w, F_matrix
+
+
+
+
+
+
+
+
